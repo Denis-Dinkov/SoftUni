571ac03f9e02d58454bdb83262913f0b67bdc6b7
Denis Dinkov <denisdinkov7@gmail.com>
2023-03-15 20:17:55 +0200

    fix second task

--- Regular Expressions/2.PhoneNumber.js
+++ Regular Expressions/2.PhoneNumber.js
@@ -3,7 +3,7 @@ function numberPhone(input) {
   let pattern = /\+359([ -])2\1\d{3}\1\d{4}\b/g;
   let result = numbers.match(pattern);
 
-  console.log(result.join());
+  console.log(result.join(", "));
 }
 numberPhone([
   "+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222,+359-2-222-222, +359-2-222-22222 +359-2-222-2222",
