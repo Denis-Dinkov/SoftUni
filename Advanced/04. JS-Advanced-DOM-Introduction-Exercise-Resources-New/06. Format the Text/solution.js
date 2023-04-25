0ca8b3a91d4fc3137ff600230718b41e145eedd1
Denis Dinkov <denisdinkov7@gmail.com>
2023-04-25 23:56:44 +0300

    task

--- Advanced/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ Advanced/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,13 @@
 function solve() {
-  //TODO
+  let input = document.getElementById('input').value;
+  let sentences = input.split('.').filter(x => x !== '');
+  let resultDiv = document.getElementById('output');
+
+  while(sentences.length > 0){
+    let text = sentences.splice(0,3).join('. ') + '.';
+    let p = document.createElement('p');
+    p.textContent = text;
+    resultDiv.appendChild(p)
+  }
+ 
 }
