a025d156c931566235ba5cdb5b2392c9cf42043e
Denis Dinkov <denisdinkov7@gmail.com>
2023-05-02 12:35:12 +0300

    tasks

--- Advanced/05. JS-Advanced-DOM-Events-Lab-Resources/02. Delete-From-Table/app.js
+++ Advanced/05. JS-Advanced-DOM-Events-Lab-Resources/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  let usersElement = document.querySelectorAll("tr td:nth-of-type(2)");
+  let inputElement = document.querySelector('input[type="text"]');
+  let resultMessageElement = document.getElementById("result");
+
+  let targetElement = Array.from(usersElement).find(
+    (x) => x.textContent == inputElement.value
+  );
+  if (targetElement) {
+    resultMessageElement.textContent = "Deleted.";
+    targetElement.parentNode.remove();
+  } else {
+    resultMessageElement.textContent = "Not found.";
+  }
+
+  inputElement.value = "";
 }
