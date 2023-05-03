67fd89d1ade80e3903a8546077f11523fb3d7801
Denis Dinkov <denisdinkov7@gmail.com>
2023-05-03 22:13:12 +0300

    tasks

--- Advanced/05. JS-Advanced-DOM-Events-Exercise-Resources/02. Time-Converter/solution.js
+++ Advanced/05. JS-Advanced-DOM-Events-Exercise-Resources/02. Time-Converter/solution.js
@@ -1,5 +1,43 @@
 function attachEventsListeners() {
-    let buttonElement = document.getElementById('daysBtn');
-    
-    console.log('TODO:...');
+  //getting input values
+  let days = document.getElementById("days");
+  let hours = document.getElementById("hours");
+  let minutes = document.getElementById("minutes");
+  let seconds = document.getElementById("seconds");
+
+  //button Elements
+  document.getElementById("daysBtn").addEventListener("click", onClick);
+  document.getElementById("hoursBtn").addEventListener("click", onClick);
+  document.getElementById("minutesBtn").addEventListener("click", onClick);
+  document.getElementById("secondsBtn").addEventListener("click", onClick);
+
+  //rotations object
+  let rotations = {
+    days: 1,
+    hours: 24,
+    minutes: 1440,
+    seconds: 86400,
+  };
+
+  function convert(value, unit) {
+    let days = value / rotations[unit];
+    console.log(days);
+
+    return {
+      days: days,
+      hours: days * rotations.hours,
+      minutes: days * rotations.minutes,
+      seconds: days * rotations.seconds,
+    };
+  }
+  //clickHandler
+  function onClick(e) {
+    let input = e.target.parentNode.querySelector('input[type="text"]');
+    let time = convert(Number(input.value), input.id);
+
+    days.value = time.days;
+    hours.value = time.hours;
+    minutes.value = time.minutes;
+    seconds.value = time.seconds;
+  }
 }
--- Advanced/05. JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
+++ Advanced/05. JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,43 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  let buttonElements = Array.from(
+    document.querySelectorAll("#container div button")
+  );
+  let decodedMessage = document.getElementsByTagName("textarea")[0];
+  let encodedMessage = document.getElementsByTagName("textarea")[1];
+
+  buttonElements.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      let buttonName = e.target.textContent;
+      if (buttonName.includes("Encode")) {
+        let inputValue = e.target.parentElement.querySelector("textarea").value;
+        let encodedText = encoding(inputValue);
+
+        console.log(encodedText);
+        encodedMessage.value = encodedText;
+        decodedMessage.value = "";
+      } else if (buttonName.includes("Decode")) {
+        let display = e.target.parentElement.querySelector("textarea");
+        let decodedText = decode(encodedMessage.value);
+        display.value = decodedText;
+      }
+    });
+  });
+
+  function encoding(inputText) {
+    let encodedResult = "";
+    for (const word of inputText) {
+      let currentCharValue = word.charCodeAt() + 1;
+      encodedResult += String.fromCharCode(currentCharValue);
+    }
+    return encodedResult;
+  }
+
+  function decode(inputText) {
+    let decodeResult = "";
+    for (const word of inputText) {
+      let currentCharValue = word.charCodeAt() - 1;
+      decodeResult += String.fromCharCode(currentCharValue);
+    }
+    return decodeResult;
+  }
 }
