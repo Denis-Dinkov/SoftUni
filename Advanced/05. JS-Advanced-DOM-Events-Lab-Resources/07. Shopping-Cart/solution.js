940004913156c5f10914d57681d1c5af7f86dae6
Denis Dinkov <denisdinkov7@gmail.com>
2023-05-04 15:03:08 +0300

    tasks

--- Advanced/05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
+++ Advanced/05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
@@ -1,14 +1,43 @@
 function solve() {
-   let products = Array.from(document.querySelectorAll('.product'));
-   let addButtonElement = document.querySelectorAll('.add-product');
-   
-   for (const product of products) {
-      product.addEventListener('click',(e) => {
-         console.log(e.target);
-         if(e.target.textContent.trim() == 'Add'){
-            console.log('da');
-            e.product
-         }
-      })
-   }
-}  
+  let textAreaElement = document.querySelector("textarea");
+  let addButtonElement = Array.from(
+    document.querySelectorAll(".product .product-add")
+  ).forEach((button) => {
+    button.addEventListener("click", addEvent);
+  });
+  let checkOutButtonElement = document
+    .querySelector(".checkout")
+    .addEventListener("click", () => {
+      let finalText = `You bought ${purchasedFoods.join(
+        ", "
+      )} for ${totalSpend.toFixed(2)}.`;
+      textAreaElement.value += finalText;
+      disableButtons();
+    });
+
+  let totalSpend = 0;
+  let purchasedFoods = [];
+
+  function addEvent(e) {
+    let productName =
+      e.currentTarget.parentElement.querySelector(".product-title").textContent;
+
+    let price = Number(
+      e.currentTarget.parentElement.querySelector(".product-line-price")
+        .textContent
+    );
+
+    totalSpend += price;
+    purchasedFoods.push(productName);
+
+    textAreaElement.value += `Added ${productName} for ${price.toFixed(
+      2
+    )} to the cart.\n`;
+  }
+
+  function disableButtons() {
+    Array.from(document.querySelectorAll("button")).forEach((button) => {
+      button.disabled = true;
+    });
+  }
+}
