b2a493bbd12589d39cd4257cfd6043a71626275b
Denis Dinkov <denisdinkov7@gmail.com>
2023-05-14 21:38:45 +0300

    .

---
+++ Advanced/Advanced Functions/calculator.js
@@ -0,0 +1,40 @@
+function area() {
+  return Math.abs(this.x * this.y);
+}
+
+function vol() {
+  return Math.abs(this.x * this.y * this.z);
+}
+
+function solve(area, vol, input) {
+  let result = [];
+  let coordinates = JSON.parse(input);
+
+  for (const line of coordinates) {
+    result.push({
+      area: area.call(line),
+      volume: vol.call(line),
+    });
+  }
+  return result;
+}
+
+console.log(
+  solve(
+    area,
+    vol,
+    `[
+
+  {"x":"10","y":"-22","z":"10"},
+  
+  {"x":"47","y":"7","z":"-5"},
+  
+  {"x":"55","y":"8","z":"0"},
+  
+  {"x":"100","y":"100","z":"100"},
+  
+  {"x":"55","y":"80","z":"250"}
+  
+  ]`
+  )
+);
