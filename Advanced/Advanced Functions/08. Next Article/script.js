cca52800e78027ef298ddebc7a2ff895d10de0c2
Denis Dinkov <denisdinkov7@gmail.com>
2023-05-19 12:38:07 +0300

    .

--- Advanced/Advanced Functions/08. Next Article/script.js
+++ Advanced/Advanced Functions/08. Next Article/script.js
@@ -1,3 +1,13 @@
-function getArticleGenerator(articles) {
-    // TODO
+function getArticleGenerator(input) {
+  let text = input;
+
+  return () => {
+    if (text.length > 0) {
+      let conatiner = document.getElementById("content");
+      let article = document.createElement("article");
+      let currentText = text.shift();
+      article.innerText = currentText;
+      conatiner.appendChild(article);
+    }
+  };
 }
