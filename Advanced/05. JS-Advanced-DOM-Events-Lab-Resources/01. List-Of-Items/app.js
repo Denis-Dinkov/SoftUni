8449abb9e5ed5bdfcd181b9bf3bc9c4752f4aa6a
Denis Dinkov <denisdinkov7@gmail.com>
2023-05-21 12:14:35 +0300

    .

--- Advanced/05. JS-Advanced-DOM-Events-Lab-Resources/01. List-Of-Items/app.js
+++
@@ -1,9 +0,0 @@
-function addItem() {
-  let inputTextElement = document.getElementById("newItemText").value;
-  let itemListElement = document.getElementById("items");
-  let newItem = document.createElement('li');
-  newItem.textContent = inputTextElement;
-  itemListElement.appendChild(newItem);
-
-  document.getElementById("newItemText").value = '';
-}
