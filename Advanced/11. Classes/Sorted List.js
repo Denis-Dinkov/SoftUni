f506ff78ee15923f8fa4806aa784ad76f2bd458d
Denis Dinkov <denisdinkov7@gmail.com>
2023-05-24 18:14:13 +0300

    tasks

---
+++ Advanced/11. Classes/Sorted List.js
@@ -0,0 +1,42 @@
+class List {
+  constructor() {
+    this.numbers = [];
+    this.size = this.numbers.length;
+  }
+
+  add(num) {
+    this.numbers.push(num);
+    this.size = this.numbers.length;
+    this.numbers.sort((a, b) => a - b);
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.numbers.length) {
+      throw new Error("Invalid index!");
+    }
+
+    this.numbers.splice(index, 1);
+    this.numbers.sort((a, b) => a - b);
+    this.size = this.numbers.length;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.numbers.length) {
+      throw new Error("Invalid index!");
+    }
+    this.numbers.sort((a, b) => a - b);
+    return this.numbers[index];
+  }
+}
+let list = new List();
+
+list.add(5);
+
+list.add(6);
+
+list.add(7);
+
+console.log(list.get(1));
+
+list.remove(1); 
+console.log(list.get(1))
