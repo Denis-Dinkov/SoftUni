0822968e0655a0d92da676a7f4688acc9617d45c
Denis Dinkov <denisdinkov7@gmail.com>
2023-06-06 17:13:09 +0300

    .

--- JS Applications/03. Remote Data and Authentication/01.Messenger/app.js
+++ JS Applications/03. Remote Data and Authentication/01.Messenger/app.js
@@ -1,5 +1,45 @@
 function attachEvents() {
-    console.log('TODO...');
+    const url = "http://localhost:3030/jsonstore/messenger";
+    const textArea = document.getElementById('messages')
+    const sendBtn = document.getElementById('submit').addEventListener('click', postNewMessage);
+    const refreshBtn = document.getElementById('refresh')
+    refreshBtn.addEventListener('click', displayMesagges)
+
+    async function postNewMessage() {
+        const authorInput = document.querySelector('input[name="author"]');
+        const messageInput = document.querySelector('input[name="content"]');
+        if (authorInput.value == '' || messageInput.value == ''){
+            alert('Please type input')
+        }
+            await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    author: authorInput.value,
+                    content: messageInput.value
+                })
+            }
+            )
+
+            refreshBtn.click()
+        
+    }
+
+    async function displayMesagges() {
+        try{
+            textArea.value = ''
+        const res = await fetch(url);
+        const data = await res.json()
+
+        textArea.value = Object.values(data).map(({ author, content }) =>
+            `${author}: ${content}`
+        ).join('\n')
+    } catch{
+        console.log('error');
+    }
+} 
 }
 
 attachEvents();
